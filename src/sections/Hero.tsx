d7528af8cb20e90fba141c9b7b931b6ad0392c7d
krish <141345008+fedupp23@users.noreply.github.com>
2024-09-09 00:10:50 +0530

    made it responsive for tablets

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,30 +1,37 @@
-import memojiImage from'@/assets/images/memoji-computer.png';
+import memojiImage from '@/assets/images/memoji-computer.png';
 import Image from 'next/image';
 import ArrowDown from '@/assets/icons/arrow-down.svg';
+
 export const HeroSection = () => {
   return (
-    <div className='py-[90px] md:py-48'>
-      <div className="container">
-        <div className='flex flex-col items-center'>
-          <Image src={memojiImage} className='size-[100px]' alt='person peeking from behind the laptop' />
+    <div className='py-[90px] overflow-hidden md:py-48 lg:py-48'>
+      <div className="container mx-auto">
+        <div className='flex flex-col items-center overflow-hidden'>
+          <Image src={memojiImage} className='w-[100px]' alt='person peeking from behind the laptop' />
           <div className='bg-gray-950 border border-gray-700 px-4 py-[8px] inline-flex items-center gap-4 rounded-lg'>
-            <div className='bg-green-500 size-2.5 rounded-full'></div>
+            <div className='bg-green-500 w-2.5 h-2.5 rounded-full'></div>
             <div className='text-sm font-medium'> Available for new projects</div>
           </div>
         </div>
-          <h1 className='font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide font-light overflow-hidden'>Building exceptional User Experiences</h1>
-          <p className='mt-4 text-center text-white/60 overflow-hidden'>I specialize in transforming designs into funtional,high-performance web applications .Let&apos;s discuss your next project</p>
-          <div className='flex flex-col items-center mt-8 gap-4'>
-            <button className='inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl'>
-              <span className='font-semibold'>EXplore My work</span>
-              <ArrowDown className='size-4' />
-            </button>
-            <button className='inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl'>
-              <span>✌️</span>
-              <span className='font-semibold'>Let&apos;s Connect</span>
-            </button>
-          </div>
+        <div className='max-w-lg mx-auto'>
+          <h1 className='font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide font-light'>
+            Building exceptional User Experiences
+          </h1>
+          <p className='mt-4 text-center text-white/60 md:text-lg'>
+            I specialize in transforming designs into functional, high-performance web applications. Let&apos;s discuss your next project.
+          </p>
+        </div>
+        <div className='flex flex-col md:flex-row justify-center items-center mt-8 gap-4'>
+          <button className='inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl'>
+            <span className='font-semibold'>Explore My Work</span>
+            <ArrowDown className='w-4 h-4' />
+          </button>
+          <button className='inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl'>
+            <span>✌️</span>
+            <span className='font-semibold'>Let&apos;s Connect</span>
+          </button>
+        </div>
       </div>
     </div>
-  )
+  );
 };
