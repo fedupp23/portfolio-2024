09eac303f63301d52c4d00bcbd85dcfd100be989
krish <141345008+fedupp23@users.noreply.github.com>
2024-09-10 22:53:36 +0530

    fixed the size property never getting used

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -5,9 +5,9 @@ export const HeroOrbit=({children ,size,rotation }:PropsWithChildren<{size:numbe
         <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
       <div className='border border-green-500 size-[800px]' style={{
         transform:`rotate(${rotation}deg)`,
-        height:`($(size)px)`,
-        width:`$(size)px`,
-      }}> {/**the outerbox animation is wrapped under another div so it would not collide with another */}
+        height:`(${size}px)`,
+        width:`${size}px`,
+      }}> 
       <div className='border border-red-500 inline-flex'> 
         {children}
       </div>
